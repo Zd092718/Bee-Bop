d954ad4ad648635aae9a9cca4db8d3baedda23b6
Zack Dowd <zdowd2796@gmail.com>
2022-09-07 13:27:08 -0600

    death animation added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,25 +6,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 1000;
+    [SerializeField] private float deathLaunch = 500;
+    [SerializeField] private Animator anim;
     private Rigidbody2D rb;
+    private BoxCollider2D collider;
+    private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (isDead == false)
         {
-            print("Jump pressed");
-            Jump();
+            if (Input.GetMouseButton(0))
+            {
+                print("Jump pressed");
+                Jump();
+            }
         }
     }
 
     private void Jump()
     {
         rb.AddForce(transform.up * jumpForce * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle" || collision.gameObject.tag == "Ground")
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.AddForce(transform.up * deathLaunch);
+        collider.enabled = false;
+        anim.SetTrigger("Die");
+        isDead = true;
     }
 }
